43ea8cb17ed70c1e5862972e0302346e22f3786c
mridul2620 <mridulsriv26@gmail.com>
2025-03-12 10:47:28 +0000

    routes added

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,29 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
-//import LoginPage from "../src/components/login/login-page";
-import HomepageContent from "@/src/components/homepage/homepage";
+import LoginPage from "../src/components/login/login-page";
 
 const Home = () => {
-  // const [showMainContent, setShowMainContent] = useState(false);
+  const [showMainContent, setShowMainContent] = useState(false);
 
-  // useEffect(() => {
-  //   // Simulate loading time for the preloader
-  //   setTimeout(() => {
-  //     setShowMainContent(true);
-  //   }, 3000); // 3 seconds
-  // }, []);
-  // const FullPageAnimation = () => (
-  //   <div style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}>
-  //     <LoginPage />
-  //   </div>
-  // );
+  useEffect(() => {
+    // Simulate loading time for the preloader
+    setTimeout(() => {
+      setShowMainContent(true);
+    }, 3000); // 3 seconds
+  }, []);
+  const FullPageAnimation = () => (
+    <div style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}>
+      <LoginPage />
+    </div>
+  );
 
   return (
     <div>
       <Head>
         <title>Chartsign</title>
       </Head>
-      <HomepageContent />
+      <LoginPage />
     </div>
   );
 };
