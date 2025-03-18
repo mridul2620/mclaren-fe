86de7a9ca50a14561e73430bd6a5f2e1c75996b8
Mridul srivastava <87673288+mridul2620@users.noreply.github.com>
2025-03-18 12:28:17 +0000

    Update circuitPage.tsx

--- src/components/circuitpage/circuitPage.tsx
+++ src/components/circuitpage/circuitPage.tsx
@@ -4,7 +4,7 @@ import { useSearchParams } from 'next/navigation';
 import dynamic from 'next/dynamic';
 import axios from 'axios';
 import './circuitPage.css';
-import PrintButton from './PrintButton';
+import PrintButton from './printButton.tsx';
 
 // Dynamically import components with SSR disabled for circuit diagrams
 const Circuit1 = dynamic(() => import('./circuit1'), { ssr: false });
